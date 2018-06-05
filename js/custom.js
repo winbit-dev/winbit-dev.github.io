47aaf8dc598bdd6bdd443f75bef57cc60c59685e
winbit-dev <37393703+winbit-dev@users.noreply.github.com>
2018-06-05 11:15:51 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -279,3 +279,50 @@ $(document).ready(function(){
 			 }, 1200, "easeInOutExpo");
 		});
 });
+
+
+jQuery('.modal').on('hidden.bs.modal', function (e) {
+  jQuery('.modal video').attr("src", jQuery(".modal  video").attr("src"));
+});
+
+
+
+function playPause() {
+	var myVideo = document.getElementsByTagName('video')[0];
+	if (myVideo.paused)
+			myVideo.play();
+	else
+			myVideo.pause();
+}
+
+function setModalMaxHeight(element) {
+  this.$element     = $(element);  
+  this.$content     = this.$element.find('.modal-content');
+  var borderWidth   = this.$content.outerHeight() - this.$content.innerHeight();
+  var dialogMargin  = $(window).width() < 768 ? 20 : 60;
+  var contentHeight = $(window).height() - (dialogMargin + borderWidth);
+  var headerHeight  = this.$element.find('.modal-header').outerHeight() || 0;
+  var footerHeight  = this.$element.find('.modal-footer').outerHeight() || 0;
+  var maxHeight     = contentHeight - (headerHeight + footerHeight);
+
+  this.$content.css({
+      'overflow': 'hidden'
+  });
+  
+  this.$element
+    .find('.modal-body').css({
+      'max-height': maxHeight,
+      'overflow-y': 'auto'
+  });
+}
+
+$('.modal').on('show.bs.modal', function() {
+  $(this).show();
+  setModalMaxHeight(this);
+});
+
+$(window).resize(function() {
+  if ($('.modal.in').length != 0) {
+    setModalMaxHeight($('.modal.in'));
+  }
+});
